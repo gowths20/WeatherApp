22dec4d82a0bcaee56541125af566f6d26461c8a
gowths20 <gowtham2indian@gmail.com>
2020-11-06 00:50:23 +0530

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component, VERSION } from "@angular/core";
+
+@Component({
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
+})
+export class AppComponent {
+  name = "Angular " + VERSION.major;
+  Arr = Array; //Array type captured in a variable
+  length: number = 9;
+}
---
+++ src/cards/card.component.ts
@@ -0,0 +1,14 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "my-card",
+  templateUrl: "./card.component.html",
+  styleUrls: ["./card.component.css"]
+})
+export class CardComponent {
+  cardData = {
+    imageUrl: "https://miro.medium.com/max/1250/0*Q2EFk7g36R0h8dAZ",
+    alt: "img",
+    selected: false
+  };
+}
