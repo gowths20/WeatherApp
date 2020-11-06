8c99baafbcfe7ae04deb6e29042d41815cc4d615
gowths20 <gowtham2indian@gmail.com>
2020-11-07 00:59:17 +0530

    3 files modified

--- src/cards/card.component.ts
+++ src/cards/card.component.ts
@@ -11,7 +11,16 @@ export class CardComponent {
       imageUrl: "https://miro.medium.com/max/1250/0*Q2EFk7g36R0h8dAZ",
       alt: "img",
       selected: false,
-      data: "",
+      data: {
+        name: "Madurai",
+        id: 701,
+        main: "Mist",
+        description: "mist",
+        temp: 298.15,
+        feels_like: 302.9,
+        humidity: 94
+      },
+      main: {},
       inputVal: ""
     },
     {
@@ -72,5 +81,8 @@ export class CardComponent {
     }
   ];
 
-  enableInput() {}
+  enableInput(index) {
+    this.cardData[index].selected = !this.cardData[index].selected;
+  }
+  updateData(index) {}
 }
